0edb243f54e63a730d5c929667c2b759f477bc04
Chou Hung-Yi <s1243221@gmail.com>
2019-08-08 01:48:47 +0800

    --

---
+++ bashtools/__init__.py
@@ -0,0 +1,1 @@
+from bashtools import *
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='bashtools',
       version='0.1',
       description='simple bash tools for python',
-      url='https://github.com/',
+      url='https://github.com/HYChou0515/bashtools/archive/master.zip',
       author='hychou',
       author_email='hychou0515@gmail.com',
       license='MIT',
-      packages=['bashtools'],
+      packages=find_packages(),
 )
